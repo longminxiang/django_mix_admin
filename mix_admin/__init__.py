59fa4e5bd6f6caa8163b3a3f51a2759c77ba1162
eric <longminxiang@163.com>
2021-08-06 10:33:14 +0800

    public to pypi

---
+++ mix_admin/__init__.py
@@ -0,0 +1,6 @@
+from . import utils
+
+__version_info__ = (0, 1, 2)
+__version__ = '.'.join([str(v) for v in __version_info__])
+
+__all__ = ['utils']
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from mix_admin import __version__ as version
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-mix-admin",
+    version=version,
+    author="Eric Long",
+    author_email="longminxiang@163.com",
+    description="django admin plus",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitee.com/ericL/django_mix_admin",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: System :: Systems Administration :: Authentication/Directory',
+    ],
+)
